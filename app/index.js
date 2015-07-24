6d8e1f0b6f54a27df2c9159b8c5446eb2403e1c7
shgtkshruch <shgtk.shruch@gmail.com>
2015-07-24 18:01:35 +0900

    Writing phase

--- app/index.js
+++ app/index.js
@@ -25,40 +25,42 @@ module.exports = generators.Base.extend({
     }.bind(this));
   },
 
-  git: function () {
-    this.copy('gitignore', '.gitignore');
-  },
+  writing: {
+    git: function () {
+      this.copy('gitignore', '.gitignore');
+    },
 
-  vagrant: function () {
-    this.vagrantIp = '192.168.33.10';
-    this.copy('Vagrantfile', 'Vagrantfile');
-  },
+    vagrant: function () {
+      this.vagrantIp = '192.168.33.10';
+      this.copy('Vagrantfile', 'Vagrantfile');
+    },
 
-  ansible: function () {
-    this.directory('ansible', 'ansible');
-    this.template('ansible/group_vars/all', 'ansible/group_vars/all');
-  },
+    ansible: function () {
+      this.directory('ansible', 'ansible');
+      this.template('ansible/group_vars/all', 'ansible/group_vars/all');
+    },
 
-  wordpress: function () {
-    mkdirp('wordpress/wp-content/themes/' + this.themeName);
-    this.copy('htaccess', 'wordpress/.htaccess');
-    this.fs.copy(
-      this.templatePath('nginx.conf.erb'),
-      this.destinationPath('wordpress/conf/nginx.conf.erb')
-    );
-  },
+    wordpress: function () {
+      mkdirp('wordpress/wp-content/themes/' + this.themeName);
+      this.copy('htaccess', 'wordpress/.htaccess');
+      this.fs.copy(
+        this.templatePath('nginx.conf.erb'),
+        this.destinationPath('wordpress/conf/nginx.conf.erb')
+      );
+    },
 
-  app: function () {
-    this.copy('index.php', 'src/index.php');
-    this.copy('header.php', 'src/header.php');
-    this.copy('footer.php', 'src/footer.php');
-    this.template('style.scss', 'src/styles/style.scss');
-    this.copy('functions.php', 'src/functions.php');
-  },
+    app: function () {
+      this.copy('index.php', 'src/index.php');
+      this.copy('header.php', 'src/header.php');
+      this.copy('footer.php', 'src/footer.php');
+      this.template('style.scss', 'src/styles/style.scss');
+      this.copy('functions.php', 'src/functions.php');
+    },
 
-  gulp: function () {
-    this.template('package.json', 'package.json', {name: _s.slugify(this.appname)});
-    this.copy('gulpfile.js', 'gulpfile.js');
+    gulp: function () {
+      this.template('package.json', 'package.json', {name: _s.slugify(this.appname)});
+      this.copy('gulpfile.js', 'gulpfile.js');
+    }
   },
 
   install: function () {
